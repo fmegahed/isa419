f870b7450ffeedf52bec07cdf9f180f02504d0d8
Fadel Megahed <fmegahed@miamioh.edu>
2024-04-02 11:24:14 -0400

    isa419 spring2024 class17

--- code_to_convert_xarigan_to_pdf_and_powerpoint.R
+++ code_to_convert_xarigan_to_pdf_and_powerpoint.R
@@ -1,15 +1,15 @@
 if(require(renderthis)==FALSE) remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
 if(require(officer) == FALSE) install.packages('officer')
 
-renderthis::to_pdf(from = 'lectures/14_regression/14_regression.html',
-                   to = 'pdfs/14_regression.pdf',
+renderthis::to_pdf(from = 'lectures/16_ml_regression/16_ml_regression.html',
+                   to = 'pdfs/16_ml_regression.pdf',
                    complex_slides = TRUE,
                    partial_slides = TRUE,
                    delay = 1)
 
 # I am using my adobe to create the ppt since the text is editable (which is more preferable to students)
-renderthis::to_pptx(from = 'lectures/14_regression/14_regression.html',
-                    to = 'ppts/14_regression.pptx',
+renderthis::to_pptx(from = 'lectures/16_ml_regression/16_ml_regression.html',
+                    to = 'ppts/16_ml_regression.pptx',
                     complex_slides = TRUE,
                     partial_slides = TRUE,
                     delay = 1)
