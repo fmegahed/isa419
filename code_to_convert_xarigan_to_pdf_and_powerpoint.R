235d360d6d7d78d39033aa767db9ed93f67a4e37
Fadel Megahed <fmegahed@miamioh.edu>
2025-01-27 21:33:30 -0500

    isa419 spring2025 class01

--- code_to_convert_xarigan_to_pdf_and_powerpoint.R
+++ code_to_convert_xarigan_to_pdf_and_powerpoint.R
@@ -1,15 +1,15 @@
 if(require(renderthis)==FALSE) remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
 if(require(officer) == FALSE) install.packages('officer')
 
-renderthis::to_pdf(from = 'lectures/21_touchalytics_lab/21_touchalytics_lab.html',
-                   to = 'pdfs/21_touchalytics_lab.pdf',
+renderthis::to_pdf(from = 'lectures/01_intro/01_intro.html',
+                   to = 'pdfs/01_intro.pdf',
                    complex_slides = TRUE,
                    partial_slides = TRUE,
                    delay = 1)
 
 # I am using my adobe to create the ppt since the text is editable (which is more preferable to students)
-renderthis::to_pptx(from = 'lectures/21_touchalytics_lab/21_touchalytics_lab.html',
-                    to = 'ppts/21_touchalytics_lab.pptx',
+renderthis::to_pptx(from = 'lectures/01_intro/01_intro.html',
+                    to = 'ppts/01_intro.pptx',
                     complex_slides = TRUE,
                     partial_slides = TRUE,
                     delay = 1)
